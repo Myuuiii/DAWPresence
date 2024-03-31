f56fe4e466c57367ae1120d605b0fc68ce652b97
Myuuiii <mute@mutedevs.nl>
2024-03-31 16:38:34 +0200

    Spectre Console Markup Changes

--- DAWPresence/Program.cs
+++ DAWPresence/Program.cs
@@ -2,6 +2,7 @@ using DAWPresence;
 
 IHost host = Host.CreateDefaultBuilder(args)
 	.UseWindowsService(options => { options.ServiceName = "DAW Rich Presence"; })
+	.ConfigureLogging(logging => { logging.ClearProviders(); })
 	.ConfigureServices(services => { services.AddHostedService<Worker>(); })
 	.Build();
 
--- DAWPresence/Worker.cs
+++ DAWPresence/Worker.cs
@@ -1,94 +1,137 @@
+using System.Net;
 using System.Reflection;
 using DiscordRPC;
+using Spectre.Console;
 using YamlDotNet.Serialization;
 
 namespace DAWPresence;
 
 internal sealed class Worker : BackgroundService
 {
-	private const string CONFIG_FILE_NAME = "config.yml";
+    private const string CONFIG_FILE_NAME = "config.yml";
 
-	private const string CREDIT = "DAWPresence by @Myuuiii#0001";
-	private readonly ILogger<Worker> _logger;
-	private readonly AppConfiguration _configuration;
-	private DiscordRpcClient? client;
-	private DateTime? startTime;
+    private const string CREDIT = "DAWPresence by @Myuuiii#0001";
+    private readonly AppConfiguration _configuration;
+    private DiscordRpcClient? client;
+    private DateTime? startTime;
 
-	public Worker(ILogger<Worker> logger)
-	{
-		_logger = logger;
-		if (File.Exists(CONFIG_FILE_NAME))
-		{
-			_logger.LogInformation("Loading configuration file...");
-			_configuration =
-				new Deserializer().Deserialize<AppConfiguration>(
-					File.ReadAllText(CONFIG_FILE_NAME));
-		}
-		else
-		{
-			_configuration = new AppConfiguration();
-			_logger.LogInformation("Creating configuration file...");
-			File.WriteAllText(CONFIG_FILE_NAME,
-				new SerializerBuilder().Build().Serialize(_configuration));
-		}
-	}
+    private const string RELEASE_VERSION = "debug-0.1.4";
 
-	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-	{
-		IEnumerable<Daw?> registeredDaws = Assembly.GetExecutingAssembly().GetTypes()
-			.Where(t => t.IsSubclassOf(typeof(Daw)))
-			.Select(t => (Daw?)Activator.CreateInstance(t));
-		IEnumerable<Daw?> regDawArray = registeredDaws as Daw[] ?? registeredDaws.ToArray();
+    public Worker(ILogger<Worker> logger)
+    {
+        VersionCheck();
 
-		foreach (Daw? r in regDawArray)
-			Console.WriteLine($"{r.DisplayName} has been registered");
+        if (File.Exists(CONFIG_FILE_NAME))
+        {
+            AnsiConsole.MarkupLine(UI.Messages.LoadedConfig);
+            _configuration =
+                new Deserializer().Deserialize<AppConfiguration>(
+                    File.ReadAllText(CONFIG_FILE_NAME));
+        }
+        else
+        {
+            _configuration = new AppConfiguration();
+            AnsiConsole.MarkupLine(UI.Messages.CreatingConfig);
+            File.WriteAllText(CONFIG_FILE_NAME,
+                new SerializerBuilder().Build().Serialize(_configuration));
+        }
+    }
 
-		while (!stoppingToken.IsCancellationRequested)
-		{
-			Daw? daw = regDawArray.FirstOrDefault(d => d.IsRunning);
-			Console.Clear();
-			if (daw is null)
-			{
-				client?.ClearPresence();
-				client?.Dispose();
-				startTime = null;
-				Console.WriteLine("No DAW is running");
-				return;
-			}
+    private static void VersionCheck()
+    {
+        if (!RELEASE_VERSION.StartsWith("beta") && !RELEASE_VERSION.Contains("debug"))
+        {
+            using (WebClient client = new())
+            {
+                try
+                {
+                    var webLatest = client.DownloadString(
+                        "https://minio.myuuiii.com/mversion/dawpresence.txt");
+                    if (webLatest != RELEASE_VERSION)
+                    {
+                        AnsiConsole.MarkupLine(string.Format(UI.Messages.f_NewVersionAvailable, RELEASE_VERSION,
+                            webLatest));
+                    }
+                }
+                catch (Exception e)
+                {
+                    AnsiConsole.MarkupLine(UI.Messages.FailedToCheckForUpdates);
+                }
+            }
+        }
+        if (RELEASE_VERSION.StartsWith("beta"))
+        {
+            AnsiConsole.MarkupLine(UI.Messages.BetaReleaseWarning);
+        }
+        if (RELEASE_VERSION.Contains("debug"))
+        {
+            AnsiConsole.MarkupLine(UI.Messages.DebugReleaseWarning);
+        }
+    }
 
-			startTime ??= DateTime.UtcNow;
-			
-			Console.WriteLine("Detected: " + daw.DisplayName);
-			Console.WriteLine("Project: " + daw.GetProjectNameFromProcessWindow());
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        IEnumerable<Daw?> registeredDaws = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t.IsSubclassOf(typeof(Daw)))
+            .Select(t => (Daw?)Activator.CreateInstance(t));
+        IEnumerable<Daw?> regDawArray = registeredDaws as Daw[] ?? registeredDaws.ToArray();
 
-			if (client is null || client.ApplicationID != daw.ApplicationId)
-			{
-				client?.ClearPresence();
-				client?.Dispose();
-				client = new DiscordRpcClient(daw.ApplicationId);
-				client.Initialize();
-			}
+        // foreach (Daw? r in regDawArray)
+        // 	AnsiConsole.MarkupLine(string.Format(UI.Messages.f_DawLoaded, r?.DisplayName));
 
-			client.SetPresence(new RichPresence
-			{
-				Details = daw.GetProjectNameFromProcessWindow() != ""
-					? _configuration.WorkingPrefixText + daw.GetProjectNameFromProcessWindow()
-					: _configuration.IdleText,
-				State = "",
-				Assets = new Assets
-				{
-					LargeImageKey = _configuration.UseCustomImage
-						? _configuration.CustomImageKey
-						: daw.ImageKey,
-					LargeImageText = CREDIT
-				},
-				Timestamps = new Timestamps
-				{
-					Start = startTime?.Add(-_configuration.Offset)
-				}
-			});
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Daw? daw = regDawArray.FirstOrDefault(d => d.IsRunning);
+            if (daw is null)
+            {
+                client?.ClearPresence();
+                client?.Dispose();
+                startTime = null;
+                AnsiConsole.MarkupLine(UI.Messages.NoDawFound);
+                return;
+            }
 
-			await Task.Delay(_configuration.UpdateInterval, stoppingToken);
-		}
-	}
+            startTime ??= DateTime.UtcNow;
+
+            AnsiConsole.MarkupLine(string.Format(UI.Messages.f_DawDetected, daw.DisplayName));
+            AnsiConsole.MarkupLine(string.Format(UI.Messages.f_ProjectDetected, daw.GetProjectNameFromProcessWindow()));
+
+            try
+            {
+                if (client is null || client.ApplicationID != daw.ApplicationId)
+                {
+                    client?.ClearPresence();
+                    client?.Dispose();
+                    client = new DiscordRpcClient(daw.ApplicationId);
+                    client.Initialize();
+                }
+
+                client.SetPresence(new RichPresence
+                {
+                    Details = daw.GetProjectNameFromProcessWindow() != ""
+                        ? _configuration.WorkingPrefixText + daw.GetProjectNameFromProcessWindow()
+                        : _configuration.IdleText,
+                    State = "",
+                    Assets = new Assets
+                    {
+                        LargeImageKey = _configuration.UseCustomImage
+                            ? _configuration.CustomImageKey
+                            : daw.ImageKey,
+                        LargeImageText = CREDIT
+                    },
+                    Timestamps = new Timestamps
+                    {
+                        Start = startTime?.Add(-_configuration.Offset)
+                    }
+                });
+            }
+            catch
+            {
+                AnsiConsole.MarkupLine(UI.Messages.DiscordRpcError);
+            }
+            
+
+            await Task.Delay(_configuration.UpdateInterval, stoppingToken);
+        }
+    }
 }
