36e660a7a7fb67116b36d693bea97dbad9d8d290
Myuuiii <mute@mutedevs.nl>
2025-06-02 09:16:28 +0200

    Lite versions of ableton

---
+++ DAWPresenceBackgroundApp/DAWs/AbletonLive11Lite.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace DAWPresence.DAWs;
+
+public partial class AbletonLive11Lite : Daw
+{
+    public AbletonLive11Lite()
+    {
+        ProcessName = "Ableton Live 11 Lite";
+        DisplayName = ProcessName;
+        ImageKey = "ableton";
+        ApplicationId = "1053952444859686983";
+        WindowTrim = " - " + DisplayName;
+        TitleOffset = 24;
+    }
+
+    public override string GetProjectNameFromProcessWindow()
+    {
+        Process? process = GetProcess();
+        if (process is null) return "";
+        string title = process.MainWindowTitle;
+        return title.Contains(WindowTrim)
+            ? TitleRegex().Match(title[..^TitleOffset]).Value
+            : "";
+    }
+
+    [GeneratedRegex("[^\\[]*")]
+    private static partial Regex TitleRegex();
+}
