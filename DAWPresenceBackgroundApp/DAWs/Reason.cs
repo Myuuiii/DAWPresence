576710c512d79725de6d8431a90d767cd1b088dd
Myuuiii <mute@mutedevs.nl>
2025-07-23 13:35:05 +0200

    Add Reason

---
+++ DAWPresenceBackgroundApp/DAWs/Reason.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace DAWPresence.DAWs;
+
+public class Reason: Daw
+{
+    public Reason()
+    {
+        ProcessName = "Reason";
+        DisplayName = "Reason";
+        ImageKey = "reason";
+        ApplicationId = "1397542364360474694";
+        WindowTrim = "";
+        TitleOffset = 0;
+    }
+
+    public override string GetProjectNameFromProcessWindow()
+    {
+        Process? process = GetProcess();
+        if (process is null) return "";
+        var title = process.MainWindowTitle;
+        var extIndex = title.LastIndexOf(".reason", StringComparison.OrdinalIgnoreCase);
+        return extIndex > 0 ? title.Substring(0, extIndex) : title;
+    }
+}
