db8658659177dc8c4108e2880b8b95fa45e8169e
Myuuiii <mute@mutedevs.nl>
2025-07-31 15:33:24 +0200

    Add Renoise and Serato DJ Pro support

---
+++ DAWPresenceBackgroundApp/DAWs/SeratoDjPro.cs
@@ -0,0 +1,18 @@
+﻿namespace DAWPresence.DAWs;
+
+public class SeratoDjPro : Daw
+{
+    public SeratoDjPro() : base()
+    {
+        ProcessName = "Serato DJ Pro";
+        DisplayName = "Serato DJ Pro";
+        ImageKey = "sdjp";
+        ApplicationId = "1400470240533545040";
+        WindowTrim = string.Empty;
+        TitleOffset = 0;
+    }
+
+    public override string GetProjectNameFromProcessWindow() =>
+        // Does not have a project name in the window title 
+        string.Empty;
+}
