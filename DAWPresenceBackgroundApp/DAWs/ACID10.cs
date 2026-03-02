752df9c73d3d52bc52ed49b4785e4b202d3c1527
Mavric Eis <Eismanx2016@outlook.com>
2026-03-02 17:33:02 -0500

    added required stuff, hopefully thats correct
    (ive never done this with github please bare with me)

---
+++ DAWPresenceBackgroundApp/DAWs/ACID10.cs
@@ -0,0 +1,24 @@
+﻿namespace DAWPresence.DAWs;
+
+public class Acid10 : Daw
+{
+    public Acid10()
+    {
+        ProcessName = "musicstudio100";
+        DisplayName = "Acid Music Studio 10";
+        ImageKey = "Acid";
+        ApplicationId = "1053779878916395048";
+        WindowTrim = " - " + DisplayName;
+        TitleOffset = 15;
+    }
+
+    public override string GetProjectNameFromProcessWindow()
+    {
+        var process = GetProcess();
+        if (process is null) return "";
+        var title = process.MainWindowTitle;
+        return title.Contains(WindowTrim)
+            ? title[..^TitleOffset]
+            : "";
+    }
+}
