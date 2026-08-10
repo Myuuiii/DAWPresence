0992ba47dcc9f9f1eca06316c376b2db3e3e9033
Simoxus <simoxusmanagement@gmail.com>
2026-08-10 03:21:07 -0600

    Add LMMS DAW support

---
+++ DAWPresence.Core/_DAWs/Lmms.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DAWPresence.DAWs;
+
+public class Lmms : Daw
+{
+    [SetsRequiredMembers]
+    public Lmms()
+    {
+        ProcessName = "lmms";
+        DisplayName = "LMMS";
+        ImageKey = "lmms";
+        ApplicationId = "1082950869856821308";
+        WindowTrim = " - LMMS";
+        TitleOffset = 0;
+    }
+
+    public override string ParseProjectName(string title)
+    {
+        return title.Contains(WindowTrim)
+            ? title[..title.IndexOf(WindowTrim, StringComparison.Ordinal)]
+            : "";
+    }
+
+    public override string GetProjectNameFromProcessWindow()
+    {
+        var process = GetProcess();
+        if (process is null) return "";
+        return ParseProjectName(process.MainWindowTitle);
+    }
+}
